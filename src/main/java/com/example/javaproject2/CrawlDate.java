c91a8dfb41c3016cdd3d29f16c499e419d2bab24
AA <hbu12221@gmail.com>
2023-04-19 14:51:26 +0900

    feat: BufferedReaderEx, BufferedReaderExTest 추가 - Buffer 기능 추가

--- src/main/java/com/example/javaproject2/CrawlDate.java
+++ src/main/java/com/example/javaproject2/CrawlDate.java
@@ -3,10 +3,8 @@ package com.example.javaproject2;
 import java.time.LocalDateTime;
 
 public class CrawlDate {
-    private LocalDateTime lastCrawlDateTime;
     // alt + insert
 
     public CrawlDate(LocalDateTime lastCrawlDateTime) {
-        this.lastCrawlDateTime = lastCrawlDateTime;
     }
 }
