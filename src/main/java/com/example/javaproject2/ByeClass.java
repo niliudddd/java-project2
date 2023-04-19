90e3023b61ce6ba9dc1051be41ec3951c2508c2f
AA <hbu12221@gmail.com>
2023-04-19 11:37:19 +0900

    feat: Bye 기능 추가 - ByeClass, ByeClassTest 추가

---
+++ src/main/java/com/example/javaproject2/ByeClass.java
@@ -0,0 +1,6 @@
+package com.example.javaproject2;
+
+public class ByeClass {
+    public void print() {
+        System.out.println("Bye");
+}
---
+++ src/main/java/com/example/javaproject2/ByeClassTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2;
+
+public class ByeClassTest {
+    public static void main(String[] args) {
+        ByeClass printbye = new ByeClass();
+        printbye.print();
+    }
+}
