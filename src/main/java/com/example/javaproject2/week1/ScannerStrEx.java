c93745a62721d69773b9f099828fa442483558b4
AA <hbu12221@gmail.com>
2023-04-20 09:43:12 +0900

    feat: ScannerStrEx, ScannerStrExTest 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+    void inputStringsAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
+
+
---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrExTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringsAndPlus();
+    }
+}
+
+
